f9441cfe28c6a64ebee697e7d03976e1dc9b7345
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-04-02 23:36:50 +0500

    Added much new logics

--- src/components/input/NewsInput.tsx
+++ src/components/input/NewsInput.tsx
@@ -1,36 +1,38 @@
-import { useRef } from "react";
-import { InputEnum } from "../../enums/inputEnum";
+import {useRef} from "react";
+import {InputEnum} from "../../enums/inputEnum";
 
 type NewsInputProps = {
-  label: string;
-  type: InputEnum;
-  onChange: (value: string) => void;
+    label: string;
+    type: InputEnum;
+    val?: string;
+    onChange: (value: string) => void;
 };
 
-const NewsInput = ({ label, type, onChange }: NewsInputProps) => {
-  const placeHolder = "";
-  const inputRef = useRef<HTMLInputElement>(null);
+const NewsInput = ({label, type, onChange, val}: NewsInputProps) => {
+    const placeHolder = "";
+    const inputRef = useRef<HTMLInputElement>(null);
 
-  const handleChange = () => {
-    if (inputRef.current) onChange(inputRef.current.value);
-  };
+    const handleChange = () => {
+        if (inputRef.current) onChange(inputRef.current.value);
+    };
 
-  return (
-    <>
-      <label className="flex flex-col gap-[15px]">
+    return (
+        <>
+            <label className="flex flex-col gap-[15px]">
         <span className="text-[#3E3232] text-[16px] inline-block capitalize font-medium">
           {label}
         </span>
-        <input
-          className="text-[#3E3232] font-medium focus:outline-none focus:ring-1 focus:ring-[#3E3232] transition-all duration-300 bg-[#F5F5F5] py-[10px] px-[16px] rounded-[12px]"
-          placeholder={placeHolder}
-          ref={inputRef}
-          type={type}
-          onChange={handleChange}
-        />
-      </label>
-    </>
-  );
+                <input
+                    defaultValue={val}
+                    className="text-[#3E3232] font-medium focus:outline-none focus:ring-1 focus:ring-[#3E3232] transition-all duration-300 bg-[#F5F5F5] py-[10px] px-[16px] rounded-[12px]"
+                    placeholder={placeHolder}
+                    ref={inputRef}
+                    type={type}
+                    onChange={handleChange}
+                />
+            </label>
+        </>
+    );
 };
 
 export default NewsInput;
