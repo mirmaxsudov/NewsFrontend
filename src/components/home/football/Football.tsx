0cf281f58e7e76c67f9bcaf50c4b54fa081b4eb9
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2025-04-06 17:25:13 +0500

    Added much new logics

--- src/components/home/football/Football.tsx
+++ src/components/home/football/Football.tsx
@@ -1,12 +1,18 @@
 import "./Football.css"
 import Calendar from "./Calendar.tsx";
+import Club from "./Club.tsx";
+import Final from "./Final.tsx";
 
 const Football = () => {
     return (
         <>
             <section className={"football__section py-[65px]"}>
                 <div className={"container max-w-container mx-auto"}>
-                    <Calendar/>
+                    <div className={"flex gap-[24px]"}>
+                        <Calendar/>
+                        <Club/>
+                        <Final/>
+                    </div>
                 </div>
             </section>
         </>
